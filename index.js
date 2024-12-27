3ec95face86db173a5aad7bee5afbc93a8de34ba
sid-max1996 <sid-max1996@yandex.ru>
2024-12-27 12:13:24 +0400

    v1.0.2 prebuilds

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const systemClock = require('./build/Release/system-clock.node')
-module.exports = systemClock
+// const systemClock = require("./build/Release/system-clock.node");
+// module.exports = systemClock;
+
+// Will load a compiled build if present or a prebuild.
+// If no build if found it will throw an exception
+const binding = require("node-gyp-build")(__dirname);
+
+module.exports = binding;
